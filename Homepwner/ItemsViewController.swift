c110866d27479de984e36c095fd478b0ca994545
albertosanchezbazan <alsaba94@hotmail.com>
2018-05-03 00:37:13 +0200

    Initial empty UITableView app

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Alberto Sánchez Bazán on 3/5/18.
+//  Copyright © 2018 Alberto Sánchez Bazán. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UITableViewController {
+    
+}
+
